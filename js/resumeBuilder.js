73c2f3be0ec33e62f6daf007c8d577e9b9755ce2
zen <zen.hardy@att.com>
2017-07-22 20:32:50 -0400

    add objects, lookup, and div content stuffers

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,67 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//Create objects and array of objects for content replacing %data% in helper.js by index.html.
+var bio = {
+	"name": "Pennywise Clown",
+	"role": "Childood Trauma Development",
+  "contacts": {
+		"mobile": "000-666-0000",
+		"email": "pennywise@weallfloatdownhere.com",
+		"github": "zenbuzz",
+		"twitter": "zenonbass",
+		"location": "Bangor, ME"
+	},
+	"welcomeMessage": "\"Beep Beep Richie!\"",
+	"skills": ["Balloons", "Joke Telling", "Eating Kids", "Sewer Remediation"],
+  "biopic": "images/me.png"
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "Clown College of Maine",
+      "location": "Bangor, ME",
+      "degree": "CBA",
+      "majors": ["Joy and Terror"],
+      "dates": "1948-1955",
+      "url": "http://www.ccom.edu"
+    },
+    {
+      "name": "Bangor Community Clown and Mime College",
+      "location": "Bangor, ME",
+      "degree": "Expelled",
+      "majors": ["Clown Management"],
+      "dates": "1947-1947",
+      "url": "http://www.bccmc.edu"
+    }
+  ],
+  "onlinecourses": [
+    {
+      "title": "Intro to Programming",
+      "school": "Udacity",
+      "dates": "2017",
+      "url": "http://www.udacity.com"
+    }
+  ]
+}
+
+var work = {
+    "jobs": [
+    {
+      "employer": "Beelzebub's Hate Emporium, Inc.",
+      "title": "Greeter",
+      "location": "7th Level, HL",
+      "dates": "0034-1957",
+      "description": "Humor development under stressful deadlines."
+    }
+  ]
+}
+
+var projects = {
+  "projects": [
+    {
+      "title": "Project Richie",
+      "dates": "1955-1986",
+      "description": "Psychological development of rural youth.",
+      "images": ["http://www.imagesiteurlhere.com"]
+		}
+	]
+}
